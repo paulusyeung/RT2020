dca0d419747b6e93faa9c5a24527ab1624f34c8e
paulus <paulusyeung@gmail.com>
2020-11-13 01:44:11 +0800

    Add: GenMemberPhoneNumbers

---
+++ RT2020.Bot/Controllers/BotController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Hangfire;
+using log4net;
+using RT2020.EF6;
+using RT2020.Bot.Helper;
+
+namespace RT2020.Bot.Controllers
+{
+    public class BotController : ApiController
+    {
+        #region Instead of naming my invoking class, I started using the following:
+        //private static log4net.ILog Log { get; set; }
+        //ILog log = log4net.LogManager.GetLogger(typeof(BotController));
+
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // In this way, I can use the same line of code in every class that uses log4net without having to remember to change code when I copy and paste.
+        // Alternatively, i could create a logging class, and have every other class inherit from my logging class.
+        // Refer: https://stackoverflow.com/questions/7089286/correct-way-of-using-log4net-logger-naming
+        #endregion
+
+        [HttpPost]
+        [Route("GenMemberPhoneNumbers/{userId}/")]
+        public IHttpActionResult GenMemberPhoneNumbers(String userId)
+        {
+            var id = Guid.Empty;
+            if (Guid.TryParse(userId, out id))
+            {
+                using (var ctx = new RT2020Entities())
+                {
+                    var staff = ctx.Staff.Where(x => x.StaffId == id && x.Status > 0).SingleOrDefault();
+                    if (staff != null)
+                    {
+                        BackgroundJob.Enqueue(() => SuperUserHelper.GenMemberPhoneNumbers(id));
+                        //SuperUserHelper.GenMemberPhoneNumbers(id);
+
+                        log.Info(String.Format("[bot, SuperUser, GenMemberPhoneNumbers] \r\nHangfire findished the Job\r\nUser = {0}", String.IsNullOrEmpty(staff.FullName) ? staff.StaffCode : staff.FullName));
+
+                        return StatusCode(HttpStatusCode.Accepted);     // 202 or use: return new StatusCodeResult(202);
+                    }
+                }
+            }
+
+            log.Info(String.Format("[bot, SuperUser, GenMemberPhoneNumbers] \r\nError found before submitting to Hangfire\r\nUser Id = {0}", userId));
+
+            return BadRequest();
+        }
+    }
+}
---
+++ RT2020.Bot/Helper/SuperUserHelper.cs
@@ -0,0 +1,244 @@
+﻿using RT2020.EF6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using VWG.Community.Util;
+
+namespace RT2020.Bot.Helper
+{
+    public class SuperUserHelper
+    {
+        public static void GenMemberPhoneNumbers(Guid id)
+        {
+            using (var ctx = new RT2020Entities())
+            {
+                ctx.Configuration.LazyLoadingEnabled = false;
+
+                #region Generate random phone numbers for each Member
+                var mbrAddresses = ctx.MemberAddress.Where(x => x.PhoneTag1Value != "" || x.PhoneTag2Value != "").ToList();
+                var counts = mbrAddresses.Count();
+                if (counts > 0)
+                {
+                    var ccodes = new CountryCodes();
+                    var codelist = ccodes.GetList();
+
+                    //using (var scope = ctx.Database.BeginTransaction())
+                    //{
+                        try
+                        {
+                            //List<String> uniqueCountries = new List<string>();
+                            int i = 0;
+                            foreach (var item in mbrAddresses)
+                            {
+                                #region 用 random gen phone number 取代原有嘅 phone number
+                                /** 假設條件（address 先決）：
+                                 * 0. 冇地址，當 others
+                                 * 1. 非 ASCII 地址就係中國
+                                 * 2. 由 Address 找到所屬 Country，分為：中國﹑香港﹑其他
+                                 * 3. 如果 match 唔倒，當係 香港
+                                 */
+                                String currentCountry = "", number1 = "", number2 = "";
+
+                                currentCountry = (String.IsNullOrEmpty(item.Address)) ?
+                                    "" :
+                                    IsChinese(item.Address) ? "china" : StripCountry(item.Address);   // 非 ASCII 地址就係中國
+                                if (currentCountry != String.Empty)
+                                {
+                                    #region 有地址
+                                    currentCountry = currentCountry.ToLower().Replace(".", "");
+
+                                    var isoCountry = ccodes.FindByDisplayName(currentCountry);     // 由 Address 找到所屬 Country
+                                    var found = isoCountry != null ? true : false;
+                                    if (!found)     // match 唔倒，當係 香港
+                                    {
+                                        currentCountry = "hong kong";
+                                        isoCountry = ccodes.FindByISO3166_Alpha3("HKG");    //! 應該加埋：FindByISO3166_Alpha2，可惜 :(
+                                    }
+                                    else
+                                {
+                                    currentCountry = isoCountry.CLDR_displayname.ToLower();
+                                }
+
+                                    var dboCountry = ctx.Country.Where(x => x.CountryCode == isoCountry.ISO3166_1_Alpha_2).FirstOrDefault();
+
+                                    #region get random Phone Numbers into: number1 number2
+                                    switch (isoCountry.ISO3166_1_Alpha_2)
+                                    {
+                                        case "CN":
+                                            number1 = (item.PhoneTag1Value.Trim() != String.Empty) ? GetRandomPhoneNumber_CN() : "";
+                                            number2 = (item.PhoneTag2Value.Trim() != String.Empty) ? GetRandomPhoneNumber_CN() : "";
+                                            break;
+                                        case "HK":
+                                            number1 = (item.PhoneTag1Value.Trim() != String.Empty) ? GetRandomPhoneNumber_HK() : "";
+                                            number2 = (item.PhoneTag2Value.Trim() != String.Empty) ? GetRandomPhoneNumber_HK() : "";
+                                            break;
+                                        default:
+                                            number1 = (item.PhoneTag1Value.Trim() != String.Empty) ? GetRandomPhoneNumber_Others() : "";
+                                            number2 = (item.PhoneTag2Value.Trim() != String.Empty) ? GetRandomPhoneNumber_Others() : "";
+                                            break;
+                                    }
+                                    #endregion
+
+                                    if (dboCountry != null) item.CountryId = dboCountry.CountryId;
+                                    item.PhoneTag1Value = number1;
+                                    item.PhoneTag2Value = number2;
+
+                                    //var inList = uniqueCountries.Where(x => x == currentCountry).FirstOrDefault();
+                                    //if (inList == null) uniqueCountries.Add(currentCountry);
+                                    #endregion
+                                }
+                                else
+                                {
+                                    #region 冇地址
+                                    number1 = (item.PhoneTag1Value.Trim() != String.Empty) ? GetRandomPhoneNumber_Others() : "";
+                                    number2 = (item.PhoneTag2Value.Trim() != String.Empty) ? GetRandomPhoneNumber_Others() : "";
+
+                                    item.PhoneTag1Value = number1;
+                                    item.PhoneTag2Value = number2;
+                                    #endregion
+                                }
+
+                                //ctx.BulkSaveChanges(options => options.BatchSize = 100);
+                                #endregion
+
+                                i++;
+                                #region SaveChanges each batch: say 100
+                                if ((i % 100) == 0)
+                                {
+                                    ctx.BulkSaveChanges();
+
+                                    // uncomment for debugging
+                                    //if (i >= 50) break;
+                                }
+                                #endregion
+                            }
+
+                            if (ctx.ChangeTracker.HasChanges())
+                            {
+                                ctx.BulkSaveChanges();
+                            }
+                            //scope.Commit();
+                        }
+                        catch (Exception ex)
+                        {
+                            //scope.Rollback();
+                            throw ex;
+                        }
+                    //}
+                }
+                #endregion
+            }
+        }
+
+        #region private methods for GenMemberPhoneNumbers
+
+        static Random rand = new Random();
+
+        /// <summary>
+        /// 假如唔係 ASCII 就當係中文
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private static bool IsChinese(String source)
+        {
+            // Encoding.ASCII.GetBytes 會將 non-ascii 轉變為 ?
+            var stripped = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(source));
+            // 如果有 ? 就當係中文
+            return (stripped.Contains("?"));
+        }
+
+        /// <summary>
+        /// 抽出最後嗰行當係 Country
+        /// 中文地址係 country 行先，所以要先用 IsChinese 過濾咗啲中文地址
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        private static String StripCountry(String source)
+        {
+            var result = "";
+            var rows = source.Split(new string[] { "\n\r" }, StringSplitOptions.RemoveEmptyEntries);
+            var lastRow = rows.Length == 0 ? "" : rows.Length > 1 ? rows[rows.Length - 1] : rows[0];
+
+            if (lastRow != String.Empty)
+            {
+                var words = lastRow.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                var lastWord = words.Length == 0 ? "" : words.Length > 1 ? words[words.Length - 1] : words[0];
+
+                // 唔要 numeric，唔要前後 spaces
+                result = Regex.Replace(lastWord, "[0-9]", "").Trim();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 其他國家：Gen 10 位數字，唔要符號
+        /// </summary>
+        /// <returns></returns>
+        private static String GetRandomPhoneNumber_Others()
+        {
+            StringBuilder telNo = new StringBuilder(10);
+            int number;
+            for (int i = 0; i < 3; i++)
+            {
+                number = rand.Next(0, 8); // digit between 0 (incl) and 8 (excl)
+                telNo = telNo.Append(number.ToString());
+            }
+            //telNo = telNo.Append("-");
+            number = rand.Next(0, 743); // number between 0 (incl) and 743 (excl)
+            telNo = telNo.Append(String.Format("{0:D3}", number));
+            //telNo = telNo.Append("-");
+            number = rand.Next(0, 10000); // number between 0 (incl) and 10000 (excl)
+            telNo = telNo.Append(String.Format("{0:D4}", number));
+            return telNo.ToString();
+        }
+
+        /// <summary>
+        /// 中國：Gen 10 位數字，唔要符號
+        /// </summary>
+        /// <returns></returns>
+        private static String GetRandomPhoneNumber_CN()
+        {
+            StringBuilder telNo = new StringBuilder(10);
+            int number = 86;
+            //telNo = telNo.Append(number.ToString());
+            //telNo = telNo.Append("-");
+            for (int i = 0; i < 3; i++)
+            {
+                number = rand.Next(0, 8); // digit between 0 (incl) and 8 (excl)
+                telNo = telNo.Append(number.ToString());
+            }
+            //telNo = telNo.Append("-");
+            number = rand.Next(0, 1000); // number between 0 (incl) and 10000 (excl)
+            telNo = telNo.Append(String.Format("{0:D3}", number));
+            //telNo = telNo.Append("-");
+            number = rand.Next(0, 10000); // number between 0 (incl) and 10000 (excl)
+            telNo = telNo.Append(String.Format("{0:D4}", number));
+            return telNo.ToString();
+        }
+
+        /// <summary>
+        /// 香港：Gen 8 位數字，唔要符號
+        /// </summary>
+        /// <returns></returns>
+        private static String GetRandomPhoneNumber_HK()
+        {
+            StringBuilder telNo = new StringBuilder(8);
+            int number = 852;
+
+            //telNo = telNo.Append(number.ToString());
+            //telNo = telNo.Append("-");
+            number = rand.Next(0, 10000); // number between 0 (incl) and 10000 (excl)
+            telNo = telNo.Append(String.Format("{0:D4}", number));
+            //telNo = telNo.Append("-");
+            number = rand.Next(0, 10000); // number between 0 (incl) and 10000 (excl)
+            telNo = telNo.Append(String.Format("{0:D4}", number));
+            return telNo.ToString();
+        }
+
+        #endregion
+    }
+}
