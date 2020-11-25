432637db7658ccad1d2b52c49b46ef40e80babb2
paulus <paulusyeung@gmail.com>
2020-11-26 06:38:53 +0800

    Add Convertor

---
+++ RT2020.EF6/Helper/Convertor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RT2020.EF6.Helper
+{
+    public class Convertor
+    {
+        /// <summary>
+        /// Convert List of EF6 Entity to DataTable
+        /// refer: https://stackoverflow.com/a/50592725
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        public static DataTable ConvertToDataTable<T>(IEnumerable<T> entities)
+        {
+            var dt = new DataTable();
+
+            //creating columns
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                dt.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            //creating rows
+            foreach (var entity in entities)
+            {
+                var values = GetObjectValues(entity);
+                dt.Rows.Add(values);
+            }
+
+
+            return dt;
+        }
+
+        public static object[] GetObjectValues<T>(T entity)
+        {
+            var values = new List<object>();
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                values.Add(prop.GetValue(entity));
+            }
+
+            return values.ToArray();
+        }
+    }
+}
