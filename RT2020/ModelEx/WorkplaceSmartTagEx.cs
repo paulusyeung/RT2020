e91f265e622c78878befbf61eab882dbccccb1ca
paulus <paulusyeung@gmail.com>
2020-11-29 01:39:29 +0800

    DAL.WorkplaceSmartTag removed

---
+++ RT2020/ModelEx/WorkplaceSmartTagEx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Gizmox.WebGUI.Forms;
+using RT2020.Helper;
+
+namespace RT2020.ModelEx
+{
+    public class WorkplaceSmartTagEx
+    {
+        public static bool DeleteTagByWorkplaceId(Guid id)
+        {
+            var result = false;
+
+            using (var ctx = new EF6.RT2020Entities())
+            {
+                using (var scope = ctx.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var list = ctx.WorkplaceSmartTag.Where(x => x.WorkplaceId == id);
+                        foreach (var item in list)
+                        {
+                            ctx.WorkplaceSmartTag.Remove(item);
+                            ctx.SaveChanges();
+                        }
+                        scope.Commit();
+                        result = true;
+                    }
+                    catch
+                    {
+                        scope.Rollback();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        #region LoadCombo
+
+        public static void LoadCombo(ref ComboBox ddList, string TextField, bool SwitchLocale)
+        {
+            LoadCombo(ref ddList, TextField, SwitchLocale, false, string.Empty, string.Empty, new string[] { TextField });
+        }
+
+        public static void LoadCombo(ref ComboBox ddList, string TextField, bool SwitchLocale, string[] OrderBy)
+        {
+            LoadCombo(ref ddList, TextField, SwitchLocale, false, string.Empty, string.Empty, OrderBy);
+        }
+
+        public static void LoadCombo(ref ComboBox ddList, string TextField, bool SwitchLocale, bool BlankLine, string BlankLineText, string WhereClause)
+        {
+            LoadCombo(ref ddList, TextField, SwitchLocale, BlankLine, BlankLineText, string.Empty, WhereClause, new String[] { TextField });
+        }
+
+        public static void LoadCombo(ref ComboBox ddList, string TextField, bool SwitchLocale, bool BlankLine, string BlankLineText, string WhereClause, string[] OrderBy)
+        {
+            LoadCombo(ref ddList, TextField, SwitchLocale, BlankLine, BlankLineText, string.Empty, WhereClause, OrderBy);
+        }
+
+        public static void LoadCombo(ref ComboBox ddList, string TextField, bool SwitchLocale, bool BlankLine, string BlankLineText, string ParentFilter, string WhereClause, string[] OrderBy)
+        {
+        //    string[] textField = { TextField };
+        //    LoadCombo(ref ddList, textField, "{0}", SwitchLocale, BlankLine, BlankLineText, ParentFilter, WhereClause, OrderBy);
+        //}
+
+        //public static void LoadCombo(ref ComboBox ddList, string[] TextField, string TextFormatString, bool SwitchLocale, bool BlankLine, string BlankLineText, string WhereClause, string[] OrderBy)
+        //{
+        //    LoadCombo(ref ddList, TextField, TextFormatString, SwitchLocale, BlankLine, BlankLineText, string.Empty, WhereClause, OrderBy);
+        //}
+
+        //public static void LoadCombo(ref ComboBox ddList, string[] TextField, string TextFormatString, bool SwitchLocale, bool BlankLine, string BlankLineText, string ParentFilter, string WhereClause, string[] OrderBy)
+        //{
+            ddList.DataSource = null;
+            ddList.Items.Clear();
+
+            #region 轉換 orderby：方便 SQL Server 做 sorting，中文字排序可參考：https://dotblogs.com.tw/jamesfu/2013/06/04/collation
+            //if (SwitchLocale && TextField[0] == OrderBy[0] && OrderBy.Length == 1)
+            if (SwitchLocale && TextField == OrderBy[0] && OrderBy.Length == 1)
+            {
+                OrderBy[0] = CookieHelper.CurrentLocaleId == LanguageHelper.AlternateLanguage2.Key ?
+                    "SmartTagValue" :
+                    CookieHelper.CurrentLocaleId == LanguageHelper.AlternateLanguage1.Key ?
+                    "SmartTagValue" :
+                    "SmartTagValue";
+            }
+            var orderby = String.Join(",", OrderBy.Select(x => "[" + x + "]"));
+            #endregion
+
+            using (var ctx = new RT2020.EF6.RT2020Entities())
+            {
+                var list = ctx.WorkplaceSmartTag.SqlQuery(
+                    String.Format(
+                        "Select * from WorkplaceSmartTag Where {0} Order By {1}",
+                        String.IsNullOrEmpty(WhereClause) ? "1 = 1" : WhereClause,
+                        orderby
+                    ))
+                    .AsNoTracking()
+                    .ToList();
+
+                #region BlankLine? 加個 blank item，置頂
+                if (BlankLine)
+                {
+                    list.Insert(0, new EF6.WorkplaceSmartTag()
+                    {
+                        SmartTagId = Guid.Empty,
+                        SmartTagValue = "",
+                    });
+                }
+                #endregion
+
+                ddList.DataSource = list;
+                ddList.ValueMember = "SmartTagId";
+                ddList.DisplayMember = !SwitchLocale ? TextField :
+                    CookieHelper.CurrentLocaleId == LanguageHelper.AlternateLanguage2.Key ?
+                    "SmartTagValue" : CookieHelper.CurrentLocaleId == LanguageHelper.AlternateLanguage1.Key ?
+                    "SmartTagValue" :
+                    "SmartTagValue";
+            }
+            if (ddList.Items.Count > 0) ddList.SelectedIndex = 0;
+        }
+
+        #endregion
+    }
+}
